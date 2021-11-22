1cdd983d968950cefe2d7fff7c4d146f8694d355
2theMoon <dlaguddh1@gmail.com>
2021-11-22 15:18:22 +0900

    ToDoList -2
    - 리스트 출력을 위해 paintToDo 함수 만들기
    - <li>, <span> 태그를 만들고 appentChild 로 배치
    - span 에는 newToDo 값을 매개변수로 받아와 저장
    - html에 만들어둔 toDoList에 다시 배치

--- todo.js
+++ todo.js
@@ -17,12 +17,24 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-
+// 핳일 리스트 폼
 function handleToDoSubmit(e) {
     e.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newTodo);
+}
 
+// 할일 리스트 출력
+function paintToDo(newTodo) {
+    // 리스트들이 담김
+    const li = document.createElement("li");
+    // 삭제 버튼을 위해 li 안에 span에 할일 목록 적는 형식
+    const span = document.createElement("span");
+    // li 안에 span 배치
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
