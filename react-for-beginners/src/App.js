82f818044d6a5b704b335d9ac25daf6e27c7587c
2theMoon <dlaguddh1@gmail.com>
2021-12-02 14:26:40 +0900

    cleanUp
    - 컴포넌트가 사라질때의 정보를 반환 할때 필요
    - 자주 사용하지는 않음

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,29 +1,24 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destoryed :(");
+  }, []);
+  return (
+    <h1>Hello</h1>
+  );
+}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("나는 처음 한번만 실행 돼요");
-  }, []);
-  useEffect(() => {
-    console.log("나는 키워드를 검색할때만 실행 돼요");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("나는 카운터가 변화할때만 실행 돼요");
-  }, [counter]);
-  useEffect(() => {
-    console.log("나는 카운터와 키워드가 변화할때만 실행 돼요");
-  }, [counter, keyword]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  }
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
