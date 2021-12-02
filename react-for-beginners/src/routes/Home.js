c37f9ccb158b44e793ac814ad78dbd5b1af3fc70
2theMoon <dlaguddh1@gmail.com>
2021-12-02 20:49:00 +0900

    Movie App part Two
    - react router 설치
    - 페이지 이동을 위해 라우터 폴더에 코드 분리

---
+++ react-for-beginners/src/routes/Home.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const json = await (await fetch(
+            `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+        )).json(); // api json 데이터 가져오기 축약 형 .then 사용과 같은 이치
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
+    useEffect(() => {
+        getMovies();
+    }, []);
+    console.log(movies);
+
+    return (
+        <div>
+            {loading ?
+                <h1>Loading...</h1>
+                :
+                <div>
+                    {movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            coverImg={movie.medium_cover_image}
+                            title={movie.title}
+                            summary={movie.summary}
+                            genres={movie.genres}
+                        />
+                    ))}
+                </div>
+            };
+        </div >
+    );
+}
+export default Home;
