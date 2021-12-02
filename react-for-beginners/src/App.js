2dd43e6e8d5641b9a8ab55e6a8a7c7a21c1229a6
2theMoon <dlaguddh1@gmail.com>
2021-12-02 15:31:51 +0900

    To Do List Part Two
    - 새로운 배열을 만들어 기존 배열에 각각에 함수를 적용
    - 쉽게 기존 배열에 인덱스들에 어떠한 함수 기능을 forEach 한것
    - 여기서 map 을 사용한 이유는 출력 때문
    - 나는 먼저 for문으로  배열을 돌아 뽑을려고 했지만 실패
    - 니코는 map을 이용해 각각의 인덱스에 <li>태그를 씌워 출력

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -9,10 +9,9 @@ function App() {
     if (toDo === "") { // 인풋 비어있으면 작동X
       return;
     }
-    setToDo(""); // 인풋값 초기화
     setToDos(currentArray => [toDo, ...currentArray]); /*[새 할일, 기존[](toDos)] 을 합쳐 새로운 배열 생성하여 갱신*/
+    setToDo(""); // 인풋값 초기화
   };
-  console.log(toDos);
   return (
     <div>
       <h1>My Yo Dos ({toDos.length})</h1>
@@ -20,6 +19,10 @@ function App() {
         <input onChange={onChange} value={toDo} type="text" placeholder="할일을 적어보세요"></input>
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
