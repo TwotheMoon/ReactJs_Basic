f12ba15df5a315bc83fd14b6678b43c51375ba68
2theMoon <dlaguddh1@gmail.com>
2021-12-02 22:35:25 +0900

    챌린지
    - 디테일 페이지에서 다시 api 에게 json 데이터 요청
    - loading 구축
    - url로 넘겨받은 id에 해당하는 정보 출력

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovies] = useState([]);
+    const { id } = useParams();
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovies(json.data.movie);
+        setLoading(false);
+        console.log(json.data.movie);
+    };
+    useEffect(() => {
+        getMovie();
+    }, []);
     return (
-        <h1>Detail</h1>
+        <div>
+            {
+                loading ? <h1>Loading...</h1> :
+                    <div>
+                        <h1>Detail {movie.title_long}</h1>
+                        <img src={movie.large_cover_image}></img>
+                        <h2>{movie.title}</h2>
+                        <p>{movie.description_full}</p>
+                        <div>{movie.genres}</div>
+                    </div>
+            }
+
+        </div>
     );
 }
 export default Detail;
