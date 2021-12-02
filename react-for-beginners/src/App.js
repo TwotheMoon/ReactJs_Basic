f3b503988c79c038a26b0398ae417197a926f921
2theMoon <dlaguddh1@gmail.com>
2021-12-02 14:09:04 +0900

    useEffect
    -두번째 인자로 오는 [] 의 경우
    추가로 코드가 실행될 조건을 적음

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,13 +4,24 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("나는 처음 한번만 실행 돼요");
   }, []);
+  useEffect(() => {
+    console.log("나는 키워드를 검색할때만 실행 돼요");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("나는 카운터가 변화할때만 실행 돼요");
+  }, [counter]);
+  useEffect(() => {
+    console.log("나는 카운터와 키워드가 변화할때만 실행 돼요");
+  }, [counter, keyword]);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
